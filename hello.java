22bb843171a8b25091acf5f82d2bc3e3cc06a067
harry <harry@0824.com>
2022-07-11 19:16:16 +0800

    test

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main(String[] args) {
 
-        System.out.println("租房系统");
+        System.out.println("租房系统1");
         System.out.println("胡泽辉！");
     }
 }
