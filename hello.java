80ee71ce00ea50724e9b7105b066a496897b6f59
whx <731858649@qq.com>
2022-07-12 09:40:38 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hello.java

--- hello.java
+++ hello.java
@@ -4,6 +4,5 @@ public class hello {
         System.out.println("租房系统111");
         System.out.println("胡泽辉！");
         System.out.println("Leon");
-        System.out.println("123123");
     }
 }
